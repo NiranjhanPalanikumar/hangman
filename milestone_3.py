5caacaa8d6010029c7d9b26e8a10030ecc0e9082
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-03-01 21:17:54 +0000

    Modifies the program to check if user's guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,8 @@
+import random
+
+word_list = ["mango", "lychee", "watermelon", "orange", "apple"]
+
+word = random.choice(word_list)
 
 while True:
     guess = input("Enter your guess (single alphabetical character): ")
@@ -6,3 +11,9 @@ while True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character")
+
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again")
