9e992c033bc5d8bf912fe7051f2360bb99429bf5
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-03-05 14:39:34 +0000

    Adds an else statement in check_guess() method for if guess is not in word

--- milestone_4.py
+++ milestone_4.py
@@ -14,6 +14,8 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word")
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again")
 
     def ask_for_input(self):
         while True:
