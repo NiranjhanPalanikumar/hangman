9837413f548a13a383cc48ab4143dfc7623185ea
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-03-05 15:27:20 +0000

    Adds an else statement in the check_guess() method to print messages if guess is not in the word

--- milestone_4.py
+++ milestone_4.py
@@ -21,8 +21,11 @@ class Hangman:
             
             self.num_letters = self.num_letters - 1
 
-        #else:
-            #print(f"Sorry, {guess} is not in the word. Try again")
+        else:
+            self.num_lives = self.num_lives - 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
 
     def ask_for_input(self):
         while True:
